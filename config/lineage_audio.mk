a4aa751cb5abb89560fbd0741cc7de50fc7e62c7
Makesh Vineeth <makeshvineeth1997@gmail.com>
2020-06-05 18:05:29 +0530

    Fix silly mistakes

--- config/lineage_audio.mk
+++ config/lineage_audio.mk
@@ -9,7 +9,7 @@ RINGTONE_PATH := vendor/lineage/prebuilt/common/media/audio/ringtones
 # Alarms
 PRODUCT_COPY_FILES += \
     $(ALARM_PATH)/CyanAlarm.ogg:system/media/audio/alarms/CyanAlarm.ogg \
-    $(ALARM_PATH)/NuclearLaunch.ogg:system/media/audio/alarms/CoolRemix.ogg
+    $(ALARM_PATH)/CoolRemix.ogg.ogg:system/media/audio/alarms/CoolRemix.ogg
 
 # Notifications
 PRODUCT_COPY_FILES += \
@@ -21,7 +21,7 @@ PRODUCT_COPY_FILES += \
     $(NOTIFICATION_PATH)/Pong.ogg:system/media/audio/notifications/Pong.ogg \
     $(NOTIFICATION_PATH)/Rang.ogg:system/media/audio/notifications/Rang.ogg \
     $(NOTIFICATION_PATH)/Stone.ogg:system/media/audio/notifications/Stone.ogg \
-    $(NOTIFICATION_PATH)/Stone.ogg:system/media/audio/notifications/iPhone.ogg
+    $(NOTIFICATION_PATH)/iPhone.ogg:system/media/audio/notifications/iPhone.ogg
 
 # Ringtones
 ifeq ($(TARGET_NEEDS_BOOSTED_SOUNDS),true)
@@ -31,7 +31,7 @@ PRODUCT_COPY_FILES += \
     $(RINGTONE_PATH)/boosted/Highscore.ogg:system/media/audio/ringtones/Highscore.ogg \
     $(RINGTONE_PATH)/boosted/Lyon.ogg:system/media/audio/ringtones/Lyon.ogg \
     $(RINGTONE_PATH)/boosted/Rockin.ogg:system/media/audio/ringtones/Rockin.ogg \
-    $(RINGTONE_PATH)/Yukaay.ogg:system/media/audio/ringtones/Flute.ogg
+    $(RINGTONE_PATH)/Flute.ogg:system/media/audio/ringtones/Flute.ogg
 else
 PRODUCT_COPY_FILES += \
     $(RINGTONE_PATH)/Boxbeat.ogg:system/media/audio/ringtones/Boxbeat.ogg \
